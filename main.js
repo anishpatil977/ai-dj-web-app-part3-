5e9b0bc279c58635f3b0a2d2302ff3421abcc44e
anishpatil977 <75301919+anishpatil977@users.noreply.github.com>
2022-01-08 14:55:10 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 song = "";
 
+leftwristX=0;
+leftwristY=0;
+rightwristX=0;
+rightwristY=0;
+
+
 function preload()
 {
 	song = loadSound("music.mp3");
@@ -13,6 +19,34 @@ function setup() {
 	video = createCapture(VIDEO);
 	video.hide();
 
+	poseNet=ml5.poseNet(video,modelLoaded);
+	poseNet.on('pose',gotPoses);
+
+}
+
+
+function modelLoaded(){
+
+console.log('poseNet is initialized ');
+
+}
+
+function gotPoses(results){
+
+if(results.length>0){
+
+console.log(results)
+
+leftwristX=results[0].pose.leftWrist.x;
+leftwristy=results[0].pose.leftWrist.y;
+console.log("leftwristX="+leftwristX+"leftwristY="+leftwristY);
+rightwristX=results[0].pose.rightWrist.x;
+rightwristY=results[0].pose.rightWrist.y;
+console.log("rightwristX="+rightwristX+"rightwristY="+rightwristY);
+
+
+}
+
 }
 
 
@@ -24,4 +58,6 @@ function draw() {
 function play()
 {
 	song.play();
+	song.setVolume(1)
+	song.rate(1)
 }
